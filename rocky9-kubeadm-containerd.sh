5cfe8925b6a826d419d50dd6b81be8ddbb3d3d0c
Lucas Gonçalves S. <scemist.lucas@gmail.com>
2024-07-24 22:48:08 -0300

    Added IP Tables Config and Calico Range Specification

--- rocky9-kubeadm-containerd.sh
+++ rocky9-kubeadm-containerd.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
 # Update dnf Repository
-
 dnf clean all
 dnf -y update
 
-# Deactivating Swap and Firewall
-
-sudo sed -i '/ swap /s/^/#/' /etc/fstab
+# Deactivating Swap
+sed -i '/ swap /s/^/#/' /etc/fstab
 mount -a # && reboot
 swapoff -a
 
+# Deactivating Firewall
 systemctl stop firewalld
 systemctl disable firewalld
 systemctl mask --now firewalld
 
-# Installing Container Runtime: ContainerD
+# Lowering Security Policies for Setup
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
+# Config Network and IpTables
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
+# Installing Container Runtime: ContainerD
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y containerd.io
 containerd config default | tee /etc/containerd/config.toml
@@ -24,13 +43,7 @@ sed -i "s/SystemdCgroup = false/SystemdCgroup = true/g"  /etc/containerd/config.
 sed -i "s/disabled_plugin/enabled_plugin/g"  /etc/containerd/config.toml
 systemctl enable --now containerd
 
-# Lowering Security Policies for Setup
-
-sudo setenforce 0
-sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
 # Installing Kubelet, Kubeadm and Kubectl
-
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -41,23 +54,16 @@ gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
 exclude=kubelet kubeadm kubectl
 EOF
 
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-sudo systemctl enable --now kubelet
-
-# Fix some net problems in kubeadm init 
-
-modprobe br_netfilter
-sysctl -p /etc/sysctl.conf
-echo 1 > /proc/sys/net/ipv4/ip_forward
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable --now kubelet
 
 # Only in the Control Plane (Master):
-
 # kubeadm init phase certs ca
 # kubeadm init phase kubeconfig admin
-# kubeadm init
+# kubeadm init --pod-network-cidr=192.168.0.0/16
 # export KUBECONFIG=/etc/kubernetes/admin.conf
 
-# kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-# kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
 
-# watch kubectl get pods -n kube-system
+# watch kubectl get pods --all-namespaces
