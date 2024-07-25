7c63d01c43aa70d6a22579c9dc5a7d5de8804866
Lucas Gonçalves S. <scemist.lucas@gmail.com>
2024-07-24 21:01:55 -0300

    Change Kubernetes Repo from Google to Official

--- rocky9-kubeadm-containerd.sh
+++ rocky9-kubeadm-containerd.sh
@@ -34,10 +34,10 @@ sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
 exclude=kubelet kubeadm kubectl
 EOF
 
